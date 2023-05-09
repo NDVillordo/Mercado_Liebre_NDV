d4e8a3ea3a96470367e908d295db8012ec0a424d
NDVillordo <nestorvillordo59@gmail.com>
2023-05-08 22:27:03 -0300

    correji app.js

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
 
     res.sendFile(path.join(__dirname+'/views/index.html'))
 
 })
+app.get('/login',(req,res)=>{
+
+    res.sendFile(path.join(__dirname+'/views/login.html'))
+
+})
+app.get('/register',(req,res)=>{
+
+    res.sendFile(path.join(__dirname+'/views/register.html'))
+
+})
 
 
-app.listen(3000,()=>{
 
-    console.log('servidor 3000 en uso');
+app.listen(PORT,()=>{
+
+    console.log('servidor'+ PORT +'en uso');
 
 });
